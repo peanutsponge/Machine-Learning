e1a06e63e5d5fb0d04a6c4e50aac680fc754d9c6
damia <damiankagenaar@gmail.com>
2022-04-01 10:15:34 +0200

    Commit before big change

--- Maintaincar_deep.py
+++ Maintaincar_deep.py
@@ -1,10 +1,11 @@
 import gym
 import numpy as np
 from tqdm import tqdm
-import matplotlib.pyplot as plt
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
+## load sequential models
+from keras.models import Sequential
+
+## load a dense layer all-to-all connection
+from keras.layers import Dense, Activation
 
 
 def StoQ(state):
@@ -12,28 +13,23 @@ def StoQ(state):
     return tuple(np.round((state - Smin) * Qdimn).astype(int))
 
 
-def create_q_model():  # copied from https://keras.io/examples/rl/deep_q_network_breakout/
-    # Network defined by the Deepmind paper
-    inputs = layers.Input(shape=Qdim)
-
-    # Convolutions on the frames on the screen
-    layer1 = layers.Conv2D(32, 8, strides=4, activation="relu")(inputs)
-    layer2 = layers.Conv2D(64, 4, strides=2, activation="relu")(layer1)
-    layer3 = layers.Conv2D(64, 3, strides=1, activation="relu")(layer2)
-
-    layer4 = layers.Flatten()(layer3)
-
-    layer5 = layers.Dense(512, activation="relu")(layer4)
-    action = layers.Dense(num_actions, activation="linear")(layer5)
-
-    return keras.Model(inputs=inputs, outputs=action)
+def create_q_model():
+    ## create model instance
+    model = Sequential([
+        Dense(10, input_dim=Qdim1 + Qdim2),
+        Activation('relu'),
+        Dense(10),
+        Activation('relu'),
+        Dense(num_actions)])
+    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=['accuracy'])
+    return model
 
 
-def QLearn(Q, d, w, eps, eps_min, num_games):
+def QLearn(w, d, ε, ε_min, num_games):
     wins = 0
     last_1p_wins = 0
 
-    eps_adj = (eps - eps_min) / num_games
+    ε_adj = (ε - ε_min) / num_games
 
     for p in tqdm(range(num_games)):
         loc = StoQ(env.reset())
@@ -41,7 +37,7 @@ def QLearn(Q, d, w, eps, eps_min, num_games):
         while not done:  # automaticaly gets satisfied after 200 actions
             Qold = Q[loc]
 
-            if np.random.random() < eps:
+            if np.random.random() < ε:
                 a = env.action_space.sample()  # pick a random
             else:
                 a = np.argmax(Qold)  # pick the optimal
@@ -57,12 +53,12 @@ def QLearn(Q, d, w, eps, eps_min, num_games):
             loc = loc_new
         wins += won
         last_1p_wins += won and p >= num_games * 0.9
-        eps -= eps_adj
+        ε -= ε_adj
 
     return wins, last_1p_wins
 
 
-def Qtest(Q, runs=5000):
+def Qtest(runs=5000):
     wins = 0
     total_reward_array = np.zeros(runs)
     for i in tqdm(range(runs)):
@@ -88,14 +84,6 @@ Qdim = [15, 10, num_actions]  # dimensions of Q-matrix
 Qdim1, Qdim2, _ = Qdim
 Qdimn = [Qdim1 - 1, Qdim2 - 1] * dS
 
-w = 0.15  # learning rate
-d = 0.95  # discount rate
-eps = 0.80  # epsilon greedy strategy
-eps_min = -3
-num_games = 2000  # number of training games
-
-optimizer = keras.optimizers.Adam(learning_rate=0.00025, clipnorm=1.0)
-loss_function = keras.losses.Huber()
 model_main = create_q_model()
 model_target = create_q_model()
 
@@ -103,9 +91,15 @@ stepsize_main = 4
 stepsize_taget = 100
 
 Q = np.random.uniform(low=-1, high=1, size=(Qdim[0], Qdim[1], Qdim[2]))  # random initial Q matrix
-wins, last_1p_wins = QLearn(Q, d, w, eps, eps_min, num_games)  # Trains a Q-matrix
+wins, last_1p_wins = QLearn(  # Trains a Q-matrix
+    w=0.15,  # learning rate
+    d=0.95,  # discount rate
+    ε=0.80,  # εilon greedy strategy
+    ε_min=-3,
+    num_games=2000  # number of training games
+)
 
 print('Training ended. Number of wins:', wins)
-print(f'Training ended. Number of wins in last {int(0.1 * num_games)}:', last_1p_wins)
-print('Test ended. Number of wins, min reward, avg reward, max reward:', Qtest(Q, runs=500))
+# print(f'Training ended. Number of wins in last {int(0.1 * num_games)}:', last_1p_wins)
+print('Test ended. Number of wins, min reward, avg reward, max reward:', Qtest(runs=500))
 env.close()  # close the openai gym environment
