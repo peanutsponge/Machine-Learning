0aa453f617ee28df2dfc16b4c8ac2c69cc6f4837
damianlaptop <d.d.kagenaar@student.utwente.nl>
2022-04-05 17:36:20 +0200

    deep edited

--- Code/deep.py
+++ Code/deep.py
@@ -1,59 +1,114 @@
 from ludo import make
 import numpy as np
-import numpy as np
-import matplotlib.pyplot as plt
-
-## does one-hot encoding for us
-from keras.utils import np_utils
-
-## load sequential models
+import tensorflow as tf
+from tqdm import tqdm
 from keras.models import Sequential
+from keras.layers import Dense, Activation, Input
 
-## load a dense layer all-to-all connection
-from keras.layers import Dense
+num_states = 4
+N1 = 8
+N2 = 10
 
-Nin = 17
-N1 = 300
-N2 = 100
-Nout = 4
-def CreateModel():
+
+def create_q_model():
     ## create model instance
     model = Sequential()
-    ## call add memeber function of model to append some layer
-    model.add(Dense(N1, input_dim=Nin, kernel_initializer='normal',
-                    activation="relu"))
-    ## add first hidden layer always match input and dimension of actual
-    ## and previous layer
-    model.add(Dense(N2, input_dim=N1,
-                    kernel_initializer="normal", activation="relu"))
-    ## add the output layer, since we are doing a classification problem
-    ## the number of output layers has to match the number of classes
-    ## since we have 10 digits 0..9 ->
-    model.add(Dense(Nout, input_dim=N2,
-                    kernel_initializer="normal", activation="softmax"))
-    ## and last keras allows us to compile the model to get all the compute power
-    ## available, and we can define a optimizer and a loss function
-    model.compile(loss="categorical_crossentropy", optimizer="adam",
-                  metrics=['accuracy'])
+    model.add(Dense(N1, input_dim=num_states, kernel_initializer='normal', activation="relu"))
+    model.add(Dense(N2, input_dim=N1, kernel_initializer="normal", activation="relu"))
+    model.add(Dense(num_actions, input_dim=N2, kernel_initializer="normal", activation="linear"))
+    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=['accuracy'])
     return model
 
 
+def action(state, ε):
+    if np.random.random() < ε:
+        a = env.action_space.sample()  # pick a random
+    else:
+        a = np.argmax(predict(state))  # pick the optimal
+    return a
+
+
+
+
+def to_state(obs, info):
+    state = [info['eyes']]
+    player = info['player']
+    opponents = [0, 1, 2, 3]
+    opponents.remove(player)
+    for pin in [0, 1, 2, 3]:
+        pin_loc = obs[player][pin]
+        state.append(pin_loc)  # TODO Check whether this is good
+        if pin_loc > 40 or pin_loc == 0:  # player pin is in hb or not on board
+            continue
+        pin_loc += 10 * player - 1
+        pin_loc %= 40
+        for opponent in opponents:
+            for opponent_pin_loc in obs[opponent]:
+                if opponent_pin_loc == 0 or opponent_pin_loc > 40:
+                    continue
+                opponent_pin_loc += 10 * opponent - 1
+                opponent_pin_loc %= 40
+                state.append(opponent_pin_loc - pin_loc)
+    if len(state) < 17:
+        #TODO maak groot genoeg
+
+
+def predict(state):
+    return model_main.predict(np.array([state]))[0]
+
+
+def QLearn(w, d, ε, ε_min, num_games):
+    wins = 0
+    ε_adj = (ε - ε_min) / num_games
+
+    for _ in tqdm(range(num_games)):
+        state = env.reset()
+        done = False
+        while not done:  # automatically gets satisfied after 200 actions
+            a = action(state, ε)
+            newstate, reward, done, info = env.step(a)  # execute the action
+            won = done and not info
+            # Change Q
+            if won:
+                Qtarget = reward
+            else:
+                Qold = predict(newstate)
+                Qnew = predict(state)
+                Qtarget = (1 - w) * Qold + w * (reward + d * np.max(Qnew))
+                state = newstate
+            model_main.fit(np.array([state]), np.array([Qtarget]), verbose=0)
+        wins += won
+        ε -= ε_adj
+    return wins
+
+
+def Qtest(runs=5000):
+    wins = 0
+    for _ in tqdm(range(runs)):
+        state = env.reset()
+        done = False
+        while not done:
+            a = np.argmax(predict(state))
+            state, reward, done, info = env.step(a)
+        wins += not info
+    return wins
+
+
+# ---------------GLOBAL VARIABLES-----------------#
 # create an instance of the game with 4 players
 env = make(num_players=4)
+num_actions = env.action_space.n
+tf.compat.v1.disable_eager_execution()
+model_main = create_q_model()
 
-# reset the game
-obs, rew, done, info = env.reset()
+wins = QLearn(  # Trains a Q-matrix
+    w=0.15,  # learning rate
+    d=0.95,  # discount rate
+    ε=0.80,  # ε greedy strategy
+    ε_min=-3,
+    num_games=1000  # number of training games
+)
 
-while True:
-    # get an action from the random player
-    action = 1
-
-    # pass the action and get the new gamestate
-    obs, rew, done, info = env.step(action)
-
-    # render for graphical representation of gamestate
-    env.render()
-
-    # quit if game is finished
-    if done:
-        break
+print('Training ended. Number of wins:', wins)
+print('Test ended. Number of wins, min reward, avg reward, max reward:', Qtest(runs=500))
+env.close()  # close the openai gym environment
--- Maintaincar_deep.py
+++ Maintaincar_deep.py
@@ -5,45 +5,50 @@ from tqdm import tqdm
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Input
 
-N1 = 8
-N2 = 10
+num_states = 2
+N1 = 24
+N2 = 48
 def create_q_model():
     ## create model instance
     model = Sequential()
-    model.add(Dense(N1, input_dim=2, kernel_initializer='normal', activation="relu"))
+    model.add(Dense(N1, input_dim=num_states, kernel_initializer='normal', activation="relu"))
     model.add(Dense(N2, input_dim=N1, kernel_initializer="normal", activation="relu"))
-    model.add(Dense(num_actions,input_dim=N2, kernel_initializer="normal", activation="linear"))
+    model.add(Dense(num_actions, input_dim=N2, kernel_initializer="normal", activation="linear"))
     model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=['accuracy'])
     return model
 
+
+def action(state, ε):
+    if np.random.random() < ε:
+        a = env.action_space.sample()  # pick a random
+    else:
+        a = np.argmax(predict(state))  # pick the optimal
+    return a
+
+
 def predict(state):
-    state_tensor = tf.convert_to_tensor(state)
-    state_tensor = tf.expand_dims(state_tensor, 0)
-    return model_main.predict(state_tensor)
+    return model_main.predict(np.array([state]))[0]
+
 
 def QLearn(w, d, ε, ε_min, num_games):
     wins = 0
     ε_adj = (ε - ε_min) / num_games
-
     for _ in tqdm(range(num_games)):
         state = env.reset()
         Qold = predict(state)
         done = False
         while not done:  # automatically gets satisfied after 200 actions
-
-            if np.random.random() < ε:
-                a = env.action_space.sample()  # pick a random
-            else:
-                a = np.argmax(Qold)  # pick the optimal
-
-            state, reward, done, info = env.step(a)  # execute the action
-            Qnew = predict(state)
+            a = action(state, ε)
+            newstate, reward, done, info = env.step(a)  # execute the action
+            Qnew = predict(newstate)
             won = done and not info
             # Change Q
-            Qtarget = reward if won else (1 - w) * Qold[a] + w * (reward + d * np.max(Qnew))
+            if won:
+                Qtarget = reward
+            else:
+                Qtarget = (1 - w) * Qold + w * (reward + d * np.max(Qnew))
             Qold = Qnew
-
-            model_main.fit(state, Qtarget, verbose=0)
+            model_main.fit(np.array([state]), np.array([Qtarget]), verbose=0)
         wins += won
         ε -= ε_adj
     return wins
@@ -51,34 +56,28 @@ def QLearn(w, d, ε, ε_min, num_games):
 
 def Qtest(runs=5000):
     wins = 0
-    total_reward_array = np.zeros(runs)
-    for i in tqdm(range(runs)):
+    for _ in tqdm(range(runs)):
         state = env.reset()
         done = False
         while not done:
             a = np.argmax(predict(state))
             state, reward, done, info = env.step(a)
-            total_reward_array[i] += reward
         wins += not info
-    return wins, np.min(total_reward_array), np.mean(total_reward_array), np.max(total_reward_array)
+    return wins
 
 
 # ---------------GLOBAL VARIABLES-----------------#
 env = gym.make('MountainCar-v0')  # selects the type of game
 num_actions = env.action_space.n
-
+tf.compat.v1.disable_eager_execution()
 model_main = create_q_model()
-# model_target = create_q_model()
-#
-# stepsize_main = 4
-# stepsize_taget = 100
 
 wins = QLearn(  # Trains a Q-matrix
     w=0.15,  # learning rate
     d=0.95,  # discount rate
-    ε=0.80,  # εilon greedy strategy
+    ε=0.80,  # ε greedy strategy
     ε_min=-3,
-    num_games=2000  # number of training games
+    num_games=200  # number of training games
 )
 
 print('Training ended. Number of wins:', wins)
