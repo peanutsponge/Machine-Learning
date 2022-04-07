ab278544140b3315d9b60d7ebcb8d6a24f8b6390
damia <damiankagenaar@gmail.com>
2022-04-07 12:09:38 +0200

    cleanup in not deep

--- Code/notdeep.py
+++ Code/notdeep.py
@@ -4,51 +4,45 @@ from tqdm import tqdm
 
 
 def StoQ(state):  # interpret the state and assign location indices inside the Q matrix
-    return tuple(np.round((state - Smin) * Qdimn).astype(int))
+    return Q[tuple(np.round((state - Smin) * Qdimn).astype(int))]
 
 
-def QLearn(Q, d, w, eps, eps_min, num_games):
-    wins = 0
+def action(Q_old, eps):
+    if np.random.random() < eps:
+        return env.action_space.sample()  # pick a random
+    else:
+        return np.argmax(Q_old)  # pick the optimal
+
+
+def QLearn(d, w, eps, eps_min, num_games):
     eps_adj = (eps - eps_min) / num_games
-
-    for _ in tqdm(range(num_games)):
-        loc = StoQ(env.reset())
-        done = False
-        while not done:  # automatically gets satisfied after 200 actions
-            Qold = Q[loc]
-
-            if np.random.random() < eps:
-                a = env.action_space.sample()  # pick a random
-            else:
-                a = np.argmax(Qold)  # pick the optimal
-
-            state, reward, done, info = env.step(a)  # execute the action
-
-            loc_new = StoQ(state)
-
-            won = done and not info
-            # Change Q
-            Qold[a] = reward if won else (1 - w) * Qold[a] + w * (reward + d * np.max(Q[loc_new]))
-
-            loc = loc_new
-        wins += won
-        eps -= eps_adj
-
-    return wins
-
-
-def Qtest(Q, runs=5000):
     wins = 0
-    for _ in tqdm(range(runs)):
+    for _ in tqdm(range(num_games)):
+        Q_old = StoQ(env.reset())
+        done = False
+        while not done:
+            a = action(Q_old, eps)
+            state, reward, done, info = env.step(a)  # execute the action
+            Q_new = StoQ(state)
+            if done and not info:  # win condition
+                wins += 1
+                Q_old[a] = reward
+            else:
+                Q_old[a] = (1 - w) * Q_old[a] + w * (reward + d * np.max(Q_new))
+            Q_old = Q_new
+        eps -= eps_adj
+    return wins / num_games
+
+
+def Qtest(num_games=500):
+    wins = 0
+    for _ in tqdm(range(num_games)):
         state = env.reset()
         done = False
-        # counter = 0
         while not done:
-            loc = StoQ(state)
-            a = np.argmax(Q[loc])
-            state, reward, done, info = env.step(a)
+            state, reward, done, info = env.step(np.argmax(StoQ(state)))
         wins += not info
-    return wins
+    return wins / num_games
 
 
 # ---------------GLOBAL VARIABLES-----------------#
@@ -62,15 +56,13 @@ Qdimn = [Qdim1 - 1, Qdim2 - 1] * dS
 
 # ---------------MAIN STUFF-----------------------#
 Q = np.random.uniform(low=-1, high=1, size=(Qdim[0], Qdim[1], Qdim[2]))  # random initial Q matrix
-wins = QLearn(  # Trains a Q-matrix
-    Q,
-    w=0.15,  # learning rate
-    d=0.95,  # discount rate
-    eps=0.80,  # epsilon greedy strategy
-    eps_min=-3,
-    num_games=2000)  # number of training games
-
-print('Training ended. Number of wins:', wins)
-print('Test ended. Number of wins:', Qtest(Q, runs=500))
+print('Training ended. Win rate:',
+      QLearn(  # Trains a Q-matrix
+          w=0.15,  # learning rate
+          d=0.95,  # discount rate
+          eps=0.80,  # epsilon greedy strategy
+          eps_min=-3,
+          num_games=1000))  # number of training games
+print('Test ended. Win rate:', Qtest(num_games=100))
 
 env.close()  # close the openai gym environment
